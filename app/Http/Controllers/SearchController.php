d8782bd0385754584748605e94933e0dd18ea5a8
nikitamakovey <nikitamakovey@yahoo.com>
2024-03-08 15:59:47 +0700

    Update search logic for API

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,6 +9,33 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 class SearchController
 {
+    const ABBREVIATIONS = [
+        'г.', 'город', 'г',
+        'ул.', 'улица', 'ул',
+        'пер.', 'переулок', 'пер',
+        'пр-т', 'проспект', 'пр-т', 'просп',
+        'б-р', 'бульвар', 'б-р', 'бул',
+        'д.', 'деревня', 'дер',
+        'х.', 'хутор', 'хут',
+        'ст.', 'станица', 'стн',
+        'а.', 'аул',
+        'пр-д', 'проезд', 'пр',
+        'ал.', 'аллея', 'алл',
+        'наб.', 'набережная', 'наб',
+        'пл.', 'площадь',
+        'туп.', 'тупик', 'туп',
+        'прлк.', 'проулок',
+        'кв.', 'квартал', 'кварт',
+        'мкр.', 'микрорайон', 'мкрн',
+        'жк', 'жилой комплекс',
+        'тер.', 'территория', 'терр',
+        'п.', 'поселок', 'пос',
+        'с.', 'село', 'сел',
+        'р. п.', 'рабочий поселок', 'рп',
+        'н. п.', 'населенный пункт', 'нп',
+        'ш.', 'шоссе', 'шос',
+    ];
+
     public function search(Request $request, Response $response): Response
     {
         $queryParams = $request->getQueryParams();
@@ -16,17 +43,55 @@ class SearchController
 
         $dbConnection = DatabaseConnection::getInstance()->getConnection();
 
-        $stmt = $dbConnection->prepare("SELECT * FROM addresses WHERE location ILIKE :query OR street ILIKE :query ORDER BY location, street, house LIMIT 5");
-        $stmt->execute(['query' => "%$searchQuery%"]);
+        $stmt = $dbConnection->prepare(
+            "SELECT * FROM addresses 
+            WHERE 
+                context ILIKE :raw_query OR (
+                    context ILIKE :filtered_query AND 
+                    house = '*'
+                )
+            ORDER BY context LIMIT 5"
+        );
+        $searchQuery = $this->getRawSearchString($searchQuery);
+        $filteredSearchQuery = $this->getFilteredSearchString($searchQuery);
+        $stmt->execute(['raw_query' => "%{$searchQuery}%", 'filtered_query' => "%{$filteredSearchQuery}%"]);
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         // Convert results to JSON
         $response->getBody()->write(json_encode(array_map(fn ($item) => [
             'branch_id' => $item['branch_id'],
-            'address' => "{$item['location']}" .
-                (in_array($item['street'], ['*', '']) ? '' : ", {$item['street']}") .
-                (in_array($item['house'], ['*', '']) ? '' : ", {$item['house']}"),
+            'address' => $item['display_address'],
         ], $results)));
         return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    private function getRawSearchString(string $searchString): string
+    {
+        $combinations = [];
+        $suggestion = mb_strtolower($searchString);
+
+        $parts = explode(',', $suggestion);
+        foreach ($parts as $part) {
+            $smallParts = explode(' ', $part);
+            foreach ($smallParts as $smallPart) {
+                if (!in_array($smallPart, self::ABBREVIATIONS) && $smallPart != '') {
+                    $combinations[] = $smallPart;
+                }
+            }
+        }
+
+        return implode(' ', $combinations);
+    }
+
+    private function getFilteredSearchString(string $searchString): string
+    {
+        $words = explode(' ', $searchString);
+        $lastWord = end($words);
+
+        if (preg_match('/\d/', $lastWord)) {
+            array_pop($words);
+            return implode(' ', $words);
+        }
+        return $searchString;
     }
 }
